9b7631ffa5362f69f28e1d0d5106c0e579f4497c
Claire Kolln <ckolln@ix-dev.cs.uoregon.edu>
2019-02-21 14:27:02 -0800

    core classes added to DB

--- source/classes.sql
+++ source/classes.sql
@@ -25,14 +25,14 @@ DROP TABLE IF EXISTS `core`;
 CREATE TABLE `core` (
   `subject` varchar(5) DEFAULT NULL,
   `num` int(11) DEFAULT NULL,
-  `crn` int(11) DEFAULT NULL,
   `name` varchar(45) DEFAULT NULL,
   `term1` varchar(2) DEFAULT NULL,
   `term2` varchar(2) DEFAULT NULL,
   `term3` varchar(2) DEFAULT NULL,
   `prereq1` varchar(7) DEFAULT NULL,
   `prereq2` varchar(7) DEFAULT NULL,
-  `prereq3` varchar(7) DEFAULT NULL
+  `prereq3` varchar(7) DEFAULT NULL,
+  `division` varchar(5) DEFAULT NULL
 ) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_0900_ai_ci;
 /*!40101 SET character_set_client = @saved_cs_client */;
 
@@ -42,6 +42,7 @@ CREATE TABLE `core` (
 
 LOCK TABLES `core` WRITE;
 /*!40000 ALTER TABLE `core` DISABLE KEYS */;
+INSERT INTO `core` VALUES ('CIS',210,'Computer Science I','f','w',NULL,'M 112',NULL,NULL,'l'),('CIS',211,'Computer Science II','w','s',NULL,'CIS 210',NULL,NULL,'l'),('CIS',212,'Computer Science III','f','s',NULL,'CIS 211',NULL,NULL,'l'),('MATH',231,'Elem of Discrete Mathematics I','f','w','s','M 112',NULL,NULL,'l'),('MATH',232,'Elem  of Discrete Mathematics II','f','w','s','M 231',NULL,NULL,'l'),('CIS',313,'Intermediate Data Structures','f','w',NULL,'CIS 212','M 232',NULL,'u'),('CIS',314,'Computer Organization','f','w',NULL,'CIS 212','M 231',NULL,'u'),('CIS',315,'Intermediate Algorithms','w','s',NULL,'CIS 213',NULL,NULL,'u'),('CIS',330,'C/C++ and Unix','w','s',NULL,'CIS 314',NULL,NULL,'u'),('CIS',415,'Operating Systems','f','s',NULL,'CIS 313','CIS 330',NULL,'u'),('CIS',422,'Software Methodology I','f','s','w','CIS 313',NULL,NULL,'u'),('CIS',425,'Principles of Programming Languages','f','s',NULL,'CIS 315',NULL,NULL,'u');
 /*!40000 ALTER TABLE `core` ENABLE KEYS */;
 UNLOCK TABLES;
 /*!40103 SET TIME_ZONE=@OLD_TIME_ZONE */;
@@ -54,4 +55,4 @@ UNLOCK TABLES;
 /*!40101 SET COLLATION_CONNECTION=@OLD_COLLATION_CONNECTION */;
 /*!40111 SET SQL_NOTES=@OLD_SQL_NOTES */;
 
--- Dump completed on 2019-02-18 15:33:58
+-- Dump completed on 2019-02-21 14:19:56
