55aed9d43a4092b921b9169295364eff87458c22
Noah Tigner <nzt@ix-dev.cs.uoregon.edu>
2019-02-22 23:42:55 -0800

    Main now prints courses for  testing

--- source/main.cpp
+++ source/main.cpp
@@ -1,10 +1,22 @@
 #include <iostream>
+#include <vector>
 #include "course.hpp"
-
+#include "courseList.hpp"
 using namespace std;
+
 
 int main() {
 	//TODO
+
+	//Populates courseList
+	CourseList *courseList = new CourseList();
+
+	//Just for testing
+	//Prints course's subject and number
+	for(auto it = courseList->getCourseList().begin(); it!=courseList->getCourseList().end(); it++) {
+		cout << (*it).getSubject() << " " << (*it).getNumber() << endl;
+	}
+
 	return 0;
 }
 
