d16368d8231a4d5984924db2f45056cd9f16a731
Claire Kolln <ckolln@ix-dev.cs.uoregon.edu>
2019-02-28 14:18:12 -0800

    removed swap files

--- source/courseList.hpp
+++ source/courseList.hpp
@@ -18,7 +18,40 @@ public:
 	//TODO
 	Course *CIS210;
 	Course *CIS211;
+	Course *CIS212;
+	Course *M232;
+	Course *M231;
+	Course *M112;
+	Course *NoPre;
+	Course *CIS313;
+	Course *CIS314;
+	Course *CIS315;
+	Course *CIS330;
+	Course *CIS415;
+	Course *CIS425;
+	Course *CIS422;
 
+	Course *CIS427;
+	Course *CIS420;
+	Course *CIS407;
+	Course *CIS410;
+	Course *CIS413;
+	Course *CIS431;
+	Course *CIS432;
+	Course *CIS433;
+	Course *CIS441;
+	Course *CIS443;
+	Course *CIS445;
+	Course *CIS451;
+	Course *CIS453;
+	Course *CIS461;
+	Course *CIS471;
+	Course *CIS472;
+
+	Course *WR320;
+	Course *WR321;
+	
+	
 
 
 
