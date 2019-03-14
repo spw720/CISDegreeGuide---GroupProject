635ece8ae0879edb1c64cbcee4dc8ab914ca511b
Joe Webb <jwebb7@uoregon.edu>
2019-03-14 13:36:26 -0700

    added next term function

--- gui/logic.cpp
+++ gui/logic.cpp
@@ -45,16 +45,45 @@ QVector<QString> required(QString trackReq) {
     }
     return req;
 }
-//QVector<QVector<QString>> path(QString term)
-QVector<QVector<QString>> path(QString term) {
-    QVector<QVector<QString>> path;
-    for(int i = 0; i < 9; i++) {
-        QVector<QString> i;
-        path.push_back(i);
+
+QVector<QString> nextTerm(QString term) { //, vector<string> taken, string track
+  QSqlQuery query;
+  QVector<QString> couldTake;
+  if (term == "fall") {
+    query.prepare("SELECT subject, number FROM not_taken WHERE fall = 1");
+  }
+  else if (term == "winter") {
+    query.prepare("SELECT subject, number FROM not_taken WHERE winter = 1");
+  }
+  else if (term == "spring") {
+    query.prepare("SELECT subject, number FROM not_taken WHERE spring = 1");
+  }
+  if (query.exec()) {
+    while (query.next()) {
+      QString s = query.value(0).toString() + " " + query.value(1).toString();
+
+      couldTake.push_back(s);
     }
+  }
+  else {
+    //ui->COURSE_OUTPUT->addItem("Invalid Read");
+      couldTake.push_back("Invalid Read");
+  }
+  return couldTake;
+}
 
-    
+//QVector<QVector<QString>> path(QString term)
+void path() {
 
 
 
+    // keep this at the end of logic to delete the path_table from the database
+    QSqlQuery del;
+    del.prepare("drop table path_table");
+    if (del.exec()) {
+        qDebug() << "Path Table Dropped";
+    }
+    else {
+        qDebug() << "Path Table Not Dropped";
+    }
 }
