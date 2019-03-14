1ea38ce29b1976e897504d0161e76264a6f778a5
Noah Tigner <Noah@Noahs-MacBook-Pro-8.local>
2019-03-13 22:14:47 -0700

    bug fix

--- gui/logic.cpp
+++ gui/logic.cpp
@@ -7,19 +7,19 @@
 QVector<QString> required(QString trackReq) {
   QSqlQuery qry;
 
-  if (trackReq == "Computational Science") {
+  if(trackReq == "Computational Science") {
       qry.prepare("SELECT sname FROM not_taken WHERE cs_track_req = 1 OR core = 1");
   }
-  else if (trackReq == "Computer Networks") {
+  else if(trackReq == "Computer Networks") {
       qry.prepare("SELECT sname FROM not_taken WHERE cn_track_req = 1 OR core = 1");
   }
-  else if (trackReq == "Computer Security") {
+  else if(trackReq == "Computer Security") {
       qry.prepare("SELECT sname FROM not_taken WHERE csec_track_req = 1 OR core = 1");
   }
-  else if (trackReq == "Computer Networks") {
+  else if(trackReq == "Computer Networks") {
       qry.prepare("SELECT sname FROM not_taken WHERE cn_track_req = 1 OR core = 1");
   }
-  else if (trackReq == "Database and Informatics") {
+  else if(trackReq == "Database and Informatics") {
       qry.prepare("SELECT sname FROM not_taken WHERE cn_track_req = 1 OR core = 1");
   }
   else {
@@ -33,7 +33,7 @@ QVector<QString> required(QString trackReq) {
       //qDebug() << "executing query ...";
       while(qry.next()) {
           //qDebug() << qry.value(0);
-          req.push_back(qry.value(0));
+          req.push_back(qry.value(0).toString);
       }
   }
   else {
