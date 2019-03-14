dc50f9260b619f3c623b6fe5befcd024ab922a05
Noah Tigner <Noah@Noahs-MacBook-Pro-8.local>
2019-03-13 22:20:44 -0700

    bug fix

--- gui/logic.cpp
+++ gui/logic.cpp
@@ -5,39 +5,40 @@
 
 
 QVector<QString> required(QString trackReq) {
-  QSqlQuery qry;
+    QSqlQuery qry;
 
-  if(trackReq == "Computational Science") {
-      qry.prepare("SELECT sname FROM not_taken WHERE cs_track_req = 1 OR core = 1");
-  }
-  else if(trackReq == "Computer Networks") {
-      qry.prepare("SELECT sname FROM not_taken WHERE cn_track_req = 1 OR core = 1");
-  }
-  else if(trackReq == "Computer Security") {
-      qry.prepare("SELECT sname FROM not_taken WHERE csec_track_req = 1 OR core = 1");
-  }
-  else if(trackReq == "Computer Networks") {
-      qry.prepare("SELECT sname FROM not_taken WHERE cn_track_req = 1 OR core = 1");
-  }
-  else if(trackReq == "Database and Informatics") {
-      qry.prepare("SELECT sname FROM not_taken WHERE cn_track_req = 1 OR core = 1");
-  }
-  else {
-      qry.prepare("SELECT sname FROM not_taken WHERE sdev_track_req = 1 OR core = 1");
-  }
+    if(trackReq == "Computational Science") {
+        qry.prepare("SELECT sname FROM not_taken WHERE cs_track_req = 1 OR core = 1");
+    }
+    else if(trackReq == "Computer Networks") {
+        qry.prepare("SELECT sname FROM not_taken WHERE cn_track_req = 1 OR core = 1");
+    }
+    else if(trackReq == "Computer Security") {
+        qry.prepare("SELECT sname FROM not_taken WHERE csec_track_req = 1 OR core = 1");
+    }
+    else if(trackReq == "Computer Networks") {
+        qry.prepare("SELECT sname FROM not_taken WHERE cn_track_req = 1 OR core = 1");
+    }
+    else if(trackReq == "Database and Informatics") {
+        qry.prepare("SELECT sname FROM not_taken WHERE cn_track_req = 1 OR core = 1");
+    }
+    else {
+        qry.prepare("SELECT sname FROM not_taken WHERE sdev_track_req = 1 OR core = 1");
+    }
 
-  //qDebug() << trackReq;
-  QVector<QString> req;
+    //qDebug() << trackReq;
+    QVector<QString> req;
 
-  if(qry.exec()) {
-      //qDebug() << "executing query ...";
-      while(qry.next()) {
-          //qDebug() << qry.value(0);
-          req.push_back(qry.value(0).toString());
-      }
-  }
-  else {
+    if(qry.exec()) {
+        //qDebug() << "executing query ...";
+        while(qry.next()) {
+            //qDebug() << qry.value(0);
+            req.push_back(qry.value(0).toString());
+        }
+    }
+    else {
       //ui->COURSE_OUTPUT->addItem("Invalid Read");
-      req.push_back("Invalid Read");
-  }
+        req.push_back("Invalid Read");
+    }
+    return req;
 }
