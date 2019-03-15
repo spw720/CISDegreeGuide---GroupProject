83498f2baea1e997bf6bef7af02e5885608cb3b1
Noah Tigner <Noah@Noahs-MacBook-Pro-8.local>
2019-03-14 18:59:05 -0700

    Max Course load is 16 credits

--- gui/logic.cpp
+++ gui/logic.cpp
@@ -103,12 +103,13 @@ QVector<QString> nextTerm(QString term,QString table) { //, vector<string> taken
           if(check.exec()) {
               QString prev = "";
               while(check.next()) {
-                  if (query.value(0).toString() != prev)
-                   {
-                    qDebug() << query.value(0);
-                    QString s = query.value(0).toString();
-                    couldTake.push_back(s);
-                    prev = query.value(0).toString();
+                  if (query.value(0).toString() != prev)  {
+                      //qDebug() << query.value(0);
+                      QString s = query.value(0).toString();
+                      if(couldTake.size() <= 4) {
+                          couldTake.push_back(s);
+                          prev = query.value(0).toString();
+                    }
                   }
               }
             }
